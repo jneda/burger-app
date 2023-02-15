2c15cbfce01e7a16faaef4fdccae2761bfbb5d34
jdane <julien.dane@gmail.com>
2023-02-15 11:29:32 +0100

    feat: cart feature complete

--- src/app.js
+++ src/app.js
@@ -2,49 +2,87 @@ const products = [
   {
     id: 1,
     name: "Burger Kid",
-    price: 5.95,
+    price: 595,
+    image: "kid.jpg",
   },
   {
     id: 2,
     name: "Artery Clogger",
-    price: 12.95,
+    price: 1295,
+    image: "clogger.jpg",
   },
   {
     id: 3,
     name: "Rabbit's Veggie",
-    price: 8.95,
+    price: 895,
+    image: "veggie.jpg",
   },
 ];
 
-function Product({ details: { name, price } }) {
+function Product(props) {
+  const { id, name, price, image } = props.details;
+  const imagePath = "public/assets/" + image;
   return (
-    <article>
+    <article className="centered" onClick={() => props.onClick(id)}>
+      <img src={imagePath} alt={name}></img>
       <h2>{name}</h2>
-      <span>{price} €</span>
+      <span>{price / 100} €</span>
     </article>
+  );
+}
+
+function Counter(props) {
+  return (
+    <div className="counter centered">
+      <button onClick={props.onRemoveProduct}>-</button>
+      <span>{props.quantity}</span>
+      <button onClick={props.onAddProduct}>+</button>
+    </div>
   );
 }
 
 function Menu(props) {
   const menuItems = props.products.map((product, index) => (
     <li key={product.id} index={index}>
-      <Product details={product} />
+      <Product
+        details={product}
+        onClick={() => props.onAddProduct(product.id)}
+      />
     </li>
   ));
 
-  return <ul className="menu">{menuItems}</ul>;
+  return (
+    <div className="menu">
+      <ul className="productList">{menuItems}</ul>
+    </div>
+  );
 }
 
-function Order(props) {
+function Cart(props) {
   const orderedItemsData = props.orderedItems || [];
   const orderedItems = orderedItemsData.map((orderedItem) => (
-    <li key={orderedItem.id}>
+    <li key={orderedItem.id} className="cartItem">
       <Product details={orderedItem} />
-      <span>{orderedItem.quantity}</span>
+      <Counter
+        quantity={orderedItem.quantity}
+        onAddProduct={() => props.onAddProduct(orderedItem.id)}
+        onRemoveProduct={() => props.onRemoveProduct(orderedItem.id)}
+      />
     </li>
   ));
 
-  return <ul className="order">{orderedItems}</ul>;
+  const totalPrice = props.totalPrice / 100;
+
+  return (
+    <div className="cart">
+      <ul className="productList">{orderedItems}</ul>
+      {totalPrice > 0 && (
+        <span className="totalPrice centered">
+          Total : {totalPrice.toFixed(2)} €
+        </span>
+      )}
+    </div>
+  );
 }
 
 class App extends React.Component {
@@ -52,22 +90,74 @@ class App extends React.Component {
     super(props);
     this.products = props.products;
     this.state = {
-      orderedItems: [{ ...this.products[1], quantity: 2 }],
+      orderedItems: [],
+      totalPrice: 0,
     };
-    console.table(this.state.orderedItems);
   }
 
-  orderItem = (e) => {
-    console.log(e.target);
+  handleAddProduct = (id) => {
+    const addedProduct = this.products.find((product) => product.id === id);
+
+    // check if product has already been added
+    const orderedItems = [...this.state.orderedItems];
+    const productIndex = orderedItems.findIndex(
+      (product) => product.id === addedProduct.id
+    );
+    // if not, add it
+    if (productIndex === -1) {
+      orderedItems.push({ ...addedProduct, quantity: 1 });
+    } else {
+      // else add 1 unit
+      orderedItems[productIndex].quantity++;
+    }
+
+    this.setState({
+      orderedItems: orderedItems,
+      totalPrice: this.getTotalPrice(orderedItems),
+    });
+    console.table(this.state);
   };
+
+  handleRemoveProduct = (id) => {
+    // find product
+    let orderedItems = [...this.state.orderedItems];
+    const removedProduct = orderedItems.find((product) => product.id === id);
+    // decrease quantity by 1
+    removedProduct.quantity--;
+    // if quantity is 0, remove product from list
+    if (removedProduct.quantity <= 0) {
+      orderedItems = orderedItems.filter((product) => product.id !== id);
+    }
+
+    this.setState({
+      orderedItems: orderedItems,
+      totalPrice: this.getTotalPrice(orderedItems),
+    });
+    console.table(this.state);
+  };
+
+  getTotalPrice(items) {
+    return items.reduce(
+      (acc, product) => acc + product.price * product.quantity,
+      0
+    );
+  }
 
   render() {
     return (
       <React.Fragment>
-        <h1>Burger App !</h1>
+        <h1>🍔 Burger App ! 🍟</h1>
         <main>
-          <Menu products={this.props.products} />
-          <Order orderedItems={this.state.orderedItems} />
+          <Menu
+            products={this.props.products}
+            onAddProduct={(id) => this.handleAddProduct(id)}
+          />
+          <Cart
+            orderedItems={this.state.orderedItems}
+            onAddProduct={(id) => this.handleAddProduct(id)}
+            onRemoveProduct={(id) => this.handleRemoveProduct(id)}
+            totalPrice={this.state.totalPrice}
+          />
         </main>
       </React.Fragment>
     );
